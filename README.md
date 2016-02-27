015b20ab8d81ccebd547e57ba81a4e8a4dcae4db
Jun Hao Xia <lightrobotex@yahoo.it>
2016-02-27 01:13:27 +0100

    Release 1.0.0

---
+++ README.md
@@ -0,0 +1,23 @@
+# FADIVVVV
+Questo UserScript ti permette di vedere gli anime su vvvvid.it senza costringerti ad avere Adobe Flash Player installato, in particolare su Mozilla Firefox.
+
+## Donazioni
+Il progetto è stato sviluppato nel mio tempo libero, ed è gratuito e senza scopo di lucro.
+
+**Se ti piace il mio lavoro, puoi sempre offrirmi una piccola donazione, quanto basta per prendermi una bibita ;)**
+
+(P.S.: No, non mi va la birra :D)
+* Bitcoin: 1ARWaQV2NmtTm7rjw7rfi51zkB5uyiZTBu
+
+## Istruzioni
+#### Prerequisito: Installa uno Script Manager (es. GreaseMonkey, TamperMonkey, NinjaKit, etc...)
+#### Passaggio 1: [Installa lo script](https://openuserjs.org/install/Robotex/FADIVVVV_-_vvvvid.it_Anti_Flash.min.user.js)
+#### Passaggio 2 (Opzionale): [Offrimi una Red Bull!](https://github.com/Robotex/FADIVVVV/blob/master/README.md#donazioni)
+
+## Domande e dubbi
+* [Per problemi tecnici andate qui](https://github.com/Robotex/FADIVVVV/issues)
+
+## Curiosità tecniche
+Lo script abilita la riproduzione dei video estendendo il supporto ai flussi HLS anche a browser che non sono compatibili con HLS nativamente. Questo avviene aggiungendo un modulo scritto da me al player projekktor utilizzato dal sito e correggendo quindi il controllo che ne indica la compatibilità.
+
+La gestione del flusso HLS è implementata dalla libreria [hls.js](https://github.com/dailymotion/hls.js/)
---
+++ fadivvvv.user.js
@@ -0,0 +1,115 @@
+// ==UserScript==
+// @name        FADIVVVV - vvvvid.it Anti Flash
+// @namespace   https://github.com/Robotex/
+// @description Guardati i tuoi anime preferiti senza avere Adobe Flash Player!
+// @author      Robotex
+// @version     1.0.0
+// @license     GPL version 3; http://www.gnu.org/copyleft/gpl.html
+// @copyright   2016+, Robotex (https://github.com/Robotex/)
+// @homepage    https://github.com/Robotex/FADIVVVV/
+// @supportURL  https://github.com/Robotex/FADIVVVV/issues
+// @match       http://www.vvvvid.it/*
+// @grant       none
+// @require     https://cdnjs.cloudflare.com/ajax/libs/hls.js/0.5.7/hls.min.js
+// ==/UserScript==
+
+jQuery(function($) {
+
+$p.newModel({
+  
+    modelId: 'VIDEOHLSJS',
+    nativeVersion: '1',
+    iLove: [
+        {ext:'m3u8', type:'application/mpegURL', platform: 'browser', streamType: ['http','httpVideo'], fixed: true},
+        {ext:'m3u8', type:'application/vnd.apple.mpegurl', platform: 'browser', streamType: ['http','httpVideo'], fixed: 'maybe'},
+        {ext:'m3u8', type:'application/x-mpegURL', platform: 'browser', streamType: ['http','httpVideo'], fixed: true}
+    ],
+    
+    applyMedia: function(destContainer) { 
+        if ($('#'+this.pp.getMediaId()+"_html").length === 0) {
+            
+            this.wasPersistent = false;
+            
+            destContainer.html('').append(
+                $('<video/>')
+                .attr({
+                    "id": this.pp.getMediaId()+"_html",         
+                    "poster": $p.utils.imageDummy(),
+                    "loop": false,
+                    "autoplay": false,
+                    "preload": "none",
+                    "x-webkit-airplay": "allow"
+                }).prop({
+                    controls: false,
+                    volume: this.getVolume()
+                }).css({
+                    'width': '100%',
+                    'height': '100%',
+                    'position': 'absolute',
+                    'top': 0,
+                    'left': 0
+                })
+            );
+        }
+
+        this.mediaElement = $('#'+this.pp.getMediaId()+"_html");
+        this.applySrc();
+    },
+    
+    _applySrc: function (e) {
+        var ref = this,
+            sources = this.getSource(),
+            b = ref.getState('AWAKENING');
+        this.mediaElement.attr('src', sources[0].src + e);
+        if (!this.isGingerbread) {
+            this.mediaElement.attr('type', sources[0].originalType)
+        }
+        this.mediaElement.bind('mousedown.projekktorqs' + this.pp.getId(), this.disableDefaultVideoElementActions);
+        this.mediaElement.bind('click.projekktorqs' + this.pp.getId(), this.disableDefaultVideoElementActions);
+        var c = function () {
+            ref.mediaElement.unbind('loadstart.projekktorqs' + ref.pp.getId());
+            ref.mediaElement.unbind('loadeddata.projekktorqs' + ref.pp.getId());
+            ref.mediaElement.unbind('canplay.projekktorqs' + ref.pp.getId());
+            ref.addListeners('error');
+            ref.addListeners('play');
+            ref.addListeners('canplay');
+            ref.mediaElement = $('#' + ref.pp.getMediaId() + '_html');
+            if (b) {
+                ref.displayReady();
+                return
+            }
+            if (ref.getSeekState('SEEKING')) {
+                if (ref._isPlaying) {
+                    ref.setPlay()
+                }
+                ref.seekedListener();
+                return
+            }
+            if (!ref.isPseudoStream) {
+                ref.setSeek(ref.media.position || 0)
+            }
+            if (ref._isPlaying) {
+                ref.setPlay()
+            }
+        };
+        if(Hls.isSupported()) {
+            var hls = new Hls();
+            hls.loadSource(this.mediaElement[0].src);
+            hls.attachMedia(this.mediaElement[0]);            
+        }
+        this.mediaElement.bind('loadstart.projekktorqs' + this.pp.getId(), c);
+        this.mediaElement.bind('loadeddata.projekktorqs' + this.pp.getId(), c);
+        this.mediaElement.bind('canplay.projekktorqs' + this.pp.getId(), c);
+        this.mediaElement[0].load();
+        if (this.isGingerbread) {
+            c();
+        }
+    },
+    
+}, 'VIDEO');
+  
+});
+    
+window.vvvvid.browserDetect.supportHLS = function () {
+  return Hls.isSupported();
+};
